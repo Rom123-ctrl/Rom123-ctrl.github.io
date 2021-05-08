27cbfc62e7e0635a3084952e783898997eb8eba9
haru <yxddc7@gmail.com>
2021-05-09 00:32:47 +0900

    Add files via upload

---
+++ works/mahjong/js/main.js
@@ -0,0 +1,71 @@
+'use strict'
+{
+  const images =[
+    'img/aka1.png',
+    'img/aka2.png',
+    'img/aka3.png',
+    'img/ji1.png',
+    'img/ji2.png',
+    'img/ji3.png',
+    'img/ji4.png',
+    'img/ji5.png',
+    'img/ji6.png',
+    'img/ji7.png',
+    'img/man1.png',
+    'img/man2.png',
+    'img/man3.png',
+    'img/man4.png',
+    'img/man5.png',
+    'img/man6.png',
+    'img/man7.png',
+    'img/man8.png',
+    'img/man9.png',
+    'img/pin1.png',
+    'img/pin2.png',
+    'img/pin3.png',
+    'img/pin4.png',
+    'img/pin5.png',
+    'img/pin6.png',
+    'img/pin7.png',
+    'img/pin8.png',
+    'img/pin9.png',
+    'img/sou1.png',
+    'img/sou2.png',
+    'img/sou3.png',
+    'img/sou4.png',
+    'img/sou5.png',
+    'img/sou6.png',
+    'img/sou7.png',
+    'img/sou8.png',
+    'img/sou9.png',
+  ];
+
+  const tileImage1 = document.getElementById('box1');
+  const tileImage2 = document.getElementById('box2');
+  const tileImage3 = document.getElementById('box3');
+  const tileImage4 = document.getElementById('box4');
+  const tileImage5 = document.getElementById('box5');
+  const tileImage6 = document.getElementById('box6');
+  const tileImage7 = document.getElementById('box7');
+  const tileImage8 = document.getElementById('box8');
+  const tileImage9 = document.getElementById('box9');
+  const tileImage10 = document.getElementById('box10');
+  const tileImage11 = document.getElementById('box11');
+  const tileImage12 = document.getElementById('box12');
+  const tileImage13 = document.getElementById('box13');
+  const tileImage14 = document.getElementById('box14');
+  tileImage1.src = images[0];
+  tileImage2.src = images[1];
+  tileImage3.src = images[2];
+  tileImage4.src = images[3];
+  tileImage5.src = images[4];
+  tileImage6.src = images[5];
+  tileImage7.src = images[6];
+  tileImage8.src = images[7];
+  tileImage9.src = images[8];
+  tileImage10.src = images[9];
+  tileImage11.src = images[10];
+  tileImage12.src = images[11];
+  tileImage13.src = images[12];
+  tileImage14.src = images[13];
+}
